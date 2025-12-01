a715e66be264f2af175379d4b3a08fbbf0451837
EC-LL\EC-5L <jodlowski79@gmail.com>
2025-12-01 10:12:07 +0100

    Codex first test

---
+++ tests/mbank.spec.ts
@@ -0,0 +1,10 @@
+// tests/mbank.spec.ts
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://www.mbank.pl/indywidualny/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/kredyty, lokaty, konta bankowe, karty, ubezpieczenia online | mBank.pl/);
+});
+
