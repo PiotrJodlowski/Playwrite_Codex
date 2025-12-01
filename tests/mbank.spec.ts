25569144d2ff8524f8b390905a9996353cfc86b4
Piotr Jodłowski <102728541+PiotrJodlowski@users.noreply.github.com>
2025-12-01 11:20:36 +0100

    Use XPath for fundusze link

--- tests/mbank.spec.ts
+++ tests/mbank.spec.ts
@@ -3,8 +3,15 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://www.mbank.pl/indywidualny/');
-
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/kredyty, lokaty, konta bankowe, karty, ubezpieczenia online | mBank.pl/);
 });
 
+test('navigates to fundusze from inwestycje i oszczędności', async ({ page }) => {
+  await page.goto('https://www.mbank.pl/indywidualny/');
+
+  await page.locator("//p[text()='inwestycje i oszczędności']").click();
+  await page.locator("//p[text()='fundusze']").click();
+
+  await expect(page).toHaveURL(/fundusze/i);
+});
